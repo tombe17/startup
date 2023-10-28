18bf5b6c29c2fbb87792f782559d526b8f07941d
tombe17 <131499102+tombe17@users.noreply.github.com>
2023-10-28 15:43:52 -0600

    Websocket

--- javascript.js
+++ javascript.js
@@ -262,6 +262,12 @@ function updateScores(userName, score, scores) {
     }
 
     return scores;
-  
 }
+
+setInterval(() => {
+    const score = Math.floor(Math.random() * 3000);
+    const chatText = document.querySelector('#player-messages');
+    chatText.innerHTML =
+      `<div class="event"><span class="player-event">Eich</span> scored ${score}</div>` + chatText.innerHTML;
+}, 5000);
 
